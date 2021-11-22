3ce0a1e77349df640a2529feedcefd825b3dc35b
the-supernova <biswalabhijit04@gmail.com>
2021-11-22 10:19:41 +0530

    Update contract to store waves

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -4,7 +4,16 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
     constructor() {
         console.log("Yo yo, I am a contract and I am smart");
+    }
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+    function getTotalWaves() public view returns (uint256) {
+        console.log("Total waves: %s", totalWaves);
+        return totalWaves;
     }
 }
