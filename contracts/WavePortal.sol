24f6f24c43b3fc63a43e196d1e84a8fbc57146fa
the-supernova <biswalabhijit04@gmail.com>
2021-11-22 11:53:01 +0530

    Add code to wave a randomPerson at me and store their walletAddress

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,15 +5,20 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
+    address[] walletAddresses;
+    mapping(address => uint256) public walletWaves;
     constructor() {
         console.log("Yo yo, I am a contract and I am smart");
     }
     function wave() public {
+        walletWaves[msg.sender] += 1;
+        walletAddresses.push(msg.sender);
         totalWaves += 1;
         console.log("%s has waved!", msg.sender);
     }
     function getTotalWaves() public view returns (uint256) {
         console.log("Total waves: %s", totalWaves);
+        console.log("Total wallets: %s", walletAddresses.length);
         return totalWaves;
     }
 }
