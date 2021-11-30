8dd966c0fa8a0e1cb6560a61e1d0c524dd04cc06
Abhijit Biswal <biswalabhijit04@gmail.com>
2021-11-30 20:57:23 +0530

    Send ETH to people waving!

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -27,6 +27,14 @@ contract WavePortal {
 
         waves.push(Wave(msg.sender, _message, block.timestamp));
         emit NewWave(msg.sender, block.timestamp, _message);
+
+        uint256 prizeAmount = 0.0001 ether;
+        require(
+            prizeAmount <= address(this).balance,
+            "Trying to withdraw more than you have"
+        );
+        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+        require(success, "Failed to withdraw money from contract.");
     }
     function getAllWaves() public view returns (Wave[] memory) {
         return waves;
